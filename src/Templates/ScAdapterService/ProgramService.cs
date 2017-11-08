b50f118e10d1840fb0702a5b769382546a8af752
Brandon Ording <bording@gmail.com>
2017-11-08 17:22:01 -0500

    Add namespace and remove DesignerCategory attribute

--- src/Templates/ScAdapterService/ProgramService.cs
+++ src/Templates/ScAdapterService/ProgramService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Linq;
 using System.ServiceProcess;
 using System.Threading.Tasks;
@@ -7,87 +6,89 @@ using NServiceBus;
 using NServiceBus.Logging;
 using ServiceControl.TransportAdapter;
 
-[DesignerCategory("Code")]
-class ProgramService : ServiceBase
+namespace ScAdapterService
 {
-    const string runAsServiceArg = "--run-as-service";
-
-    ITransportAdapter adapter;
-
-    static ILog logger;
-
-    static ProgramService()
+    class ProgramService : ServiceBase
     {
-        //TODO: optionally choose a custom logging library
-        // https://docs.particular.net/nservicebus/logging/#custom-logging
-        // LogManager.Use<TheLoggingFactory>();
-        logger = LogManager.GetLogger<ProgramService>();
-    }
+        const string runAsServiceArg = "--run-as-service";
 
-    static void Main(string[] args)
-    {
-        using (var service = new ProgramService())
+        ITransportAdapter adapter;
+
+        static ILog logger;
+
+        static ProgramService()
         {
-            // pass argument at command line to run as a windows service
-            if (args.Contains(runAsServiceArg))
-            {
-                Run(service);
-                return;
-            }
-
-            Console.Title = "ScAdapterService";
-            Console.CancelKeyPress += (sender, e) => { service.OnStop(); };
-            service.OnStart(null);
-            Console.WriteLine("\r\nPress enter key to stop program\r\n");
-            Console.Read();
-            service.OnStop();
+            //TODO: optionally choose a custom logging library
+            // https://docs.particular.net/nservicebus/logging/#custom-logging
+            // LogManager.Use<TheLoggingFactory>();
+            logger = LogManager.GetLogger<ProgramService>();
         }
-    }
 
-    protected override void OnStart(string[] args)
-    {
-        AsyncOnStart().GetAwaiter().GetResult();
-    }
-
-    async Task AsyncOnStart()
-    {
-        try
+        static void Main(string[] args)
         {
-            var adapterConfig = new TransportAdapterConfig<LearningTransport, LearningTransport>("TransportAdapter.WindowsService");
-
-            adapterConfig.CustomizeEndpointTransport(t =>
+            using (var service = new ProgramService())
             {
+                // pass argument at command line to run as a windows service
+                if (args.Contains(runAsServiceArg))
+                {
+                    Run(service);
+                    return;
+                }
+
+                Console.Title = "ScAdapterService";
+                Console.CancelKeyPress += (sender, e) => { service.OnStop(); };
+                service.OnStart(null);
+                Console.WriteLine("\r\nPress enter key to stop program\r\n");
+                Console.Read();
+                service.OnStop();
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            AsyncOnStart().GetAwaiter().GetResult();
+        }
+
+        async Task AsyncOnStart()
+        {
+            try
+            {
+                var adapterConfig = new TransportAdapterConfig<LearningTransport, LearningTransport>("TransportAdapter.WindowsService");
+
+                adapterConfig.CustomizeEndpointTransport(t =>
+                {
                 //TODO: Customize the endpoint-facing side of the adapter
                 //Use exactly same settings as in regular endpoints
             });
 
-            adapterConfig.CustomizeServiceControlTransport(t =>
-            {
+                adapterConfig.CustomizeServiceControlTransport(t =>
+                {
                 //TODO: Customize the ServiceControl-facing side of the adapter
                 //e.g. specify the same connection string as ServiceControl uses.
             });
 
-            adapter = TransportAdapter.Create(adapterConfig);
+                adapter = TransportAdapter.Create(adapterConfig);
 
-            await adapter.Start().ConfigureAwait(false);
+                await adapter.Start().ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                Exit("Failed to start", exception);
+            }
         }
-        catch (Exception exception)
+
+        void Exit(string failedToStart, Exception exception)
         {
-            Exit("Failed to start", exception);
+            logger.Fatal(failedToStart, exception);
+
+            //TODO: When using an external logging framework it is important to flush any pending entries prior to calling FailFast
+            // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
+            Environment.FailFast(failedToStart, exception);
         }
-    }
 
-    void Exit(string failedToStart, Exception exception)
-    {
-        logger.Fatal(failedToStart, exception);
-
-        //TODO: When using an external logging framework it is important to flush any pending entries prior to calling FailFast
-        // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
-        Environment.FailFast(failedToStart, exception);
-    }
-
-    protected override void OnStop()
-    {
-        adapter?.Stop().GetAwaiter().GetResult();
+        protected override void OnStop()
+        {
+            adapter?.Stop().GetAwaiter().GetResult();
+        }
     }
 }
