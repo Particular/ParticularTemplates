10696dee1d13b9191abe148f5da85210ece462dd
Brandon Ording <bording@gmail.com>
2018-11-15 17:40:54 -0500

    Update tests to actually use a different framework

--- src/Tests/TemplateTests.cs
+++ src/Tests/TemplateTests.cs
@@ -47,7 +47,7 @@ public class TemplateTests : IDisposable
         var targetDirectory = ProjectDirectory.GetSandboxPath(nameof(NServiceBusWindowsServiceDiffFramework));
         VerifyAndBuild("nsbwinservice", targetDirectory, new Dictionary<string, string>
         {
-            {"framework", "net462"}
+            {"framework", "net472"}
         });
     }
 
@@ -71,7 +71,7 @@ public class TemplateTests : IDisposable
         var targetDirectory = ProjectDirectory.GetSandboxPath(nameof(ScAdapterServiceDiffFramework));
         VerifyAndBuild("scadapterwinservice", targetDirectory, new Dictionary<string, string>
         {
-            {"framework", "net462"}
+            {"framework", "net472"}
         });
     }
 
