36de7197f9efd6fbaafe3f6b4ae44926ec53184a
David Boike <david.boike@gmail.com>
2023-05-26 07:49:31 -0500

    Apply editorconfigs during test and remove unnecessary Directory.Build.props

--- src/Tests/ProjectDirectory.cs
+++ src/Tests/ProjectDirectory.cs
@@ -6,8 +6,18 @@ public static class ProjectDirectory
 {
     static ProjectDirectory()
     {
-        ProjectPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..");
-        SandboxPath = Path.Combine(ProjectPath, "..", "..", "tempstorage", "sandbox");
+        ProjectPath = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", ".."));
+        var srcPath = Path.GetFullPath(Path.Combine(ProjectPath, ".."));
+        var templatesPath = Path.Combine(srcPath, "Templates");
+        var tempStorage = Path.GetFullPath(Path.Combine(ProjectPath, "..", "..", "tempstorage"));
+        SandboxPath = Path.Combine(tempStorage, "sandbox");
+
+        // Copy company-standard editorconfig
+        Directory.CreateDirectory(SandboxPath);
+        File.Copy(Path.Combine(srcPath, ".editorconfig"), Path.Combine(tempStorage, ".editorconfig"), true);
+
+        // Copy editorconfig overrides
+        File.Copy(Path.Combine(templatesPath, ".editorconfig"), Path.Combine(SandboxPath, ".editorconfig"), true);
     }
 
     public static string GetSandboxPath()
