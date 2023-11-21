3bcd29953b71b965f4bf2877d102198b3babe5a3
Brandon Ording <bording@gmail.com>
2023-11-21 17:34:20 -0500

    Use file scoped namespaces

--- src/ParticularTemplates/Handler/ClassName.cs
+++ src/ParticularTemplates/Handler/ClassName.cs
@@ -1,18 +1,17 @@
 using Microsoft.Extensions.Logging;
 
-namespace NamespaceName
+namespace NamespaceName;
+
+internal class ClassName(ILogger<ClassName> log) : IHandleMessages<MessageType>
 {
-    internal class ClassName(ILogger<ClassName> log) : IHandleMessages<MessageType>
+    public async Task Handle(MessageType message, IMessageHandlerContext context)
     {
-        public async Task Handle(MessageType message, IMessageHandlerContext context)
-        {
-            // Business logic here
+        // Business logic here
 
-            // Sending commands: https://docs.particular.net/nservicebus/messaging/send-a-message#inside-the-incoming-message-processing-pipeline
-            // await context.Send(...);
+        // Sending commands: https://docs.particular.net/nservicebus/messaging/send-a-message#inside-the-incoming-message-processing-pipeline
+        // await context.Send(...);
 
-            // Publishing events https://docs.particular.net/nservicebus/messaging/publish-subscribe/publish-handle-event
-            // await context.Publish(...);
-        }
+        // Publishing events https://docs.particular.net/nservicebus/messaging/publish-subscribe/publish-handle-event
+        // await context.Publish(...);
     }
 }
--- src/ParticularTemplates/Saga/SagaName.cs
+++ src/ParticularTemplates/Saga/SagaName.cs
@@ -1,57 +1,56 @@
 using Microsoft.Extensions.Logging;
 
-namespace NamespaceName
+namespace NamespaceName;
+
+internal class SagaName(ILogger<SagaName> log) : Saga<SagaNameData>,
+    IAmStartedByMessages<MessageType1>,
+    IAmStartedByMessages<MessageType2>,
+    IHandleTimeouts<MyCustomTimeout>
 {
-    internal class SagaName(ILogger<SagaName> log) : Saga<SagaNameData>,
-        IAmStartedByMessages<MessageType1>,
-        IAmStartedByMessages<MessageType2>,
-        IHandleTimeouts<MyCustomTimeout>
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaNameData> mapper)
     {
-        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaNameData> mapper)
-        {
-            // https://docs.particular.net/nservicebus/sagas/message-correlation
-            mapper.MapSaga(saga => saga.CorrelationId)
-                .ToMessage<MessageType1>(message => message.CorrelationId)
-                .ToMessage<MessageType2>(message => message.CorrelationId);
-        }
-
-        public async Task Handle(MessageType1 message, IMessageHandlerContext context)
-        {
-            // Business logic here
-        }
-
-        public async Task Handle(MessageType2 message, IMessageHandlerContext context)
-        {
-            // Update saga data: https://docs.particular.net/nservicebus/sagas/#long-running-means-stateful
-            // this.Data.Property = ...
-
-            // Sending commands: https://docs.particular.net/nservicebus/messaging/send-a-message#inside-the-incoming-message-processing-pipeline
-            // await context.Send(...);
-
-            // Publishing events https://docs.particular.net/nservicebus/messaging/publish-subscribe/publish-handle-event
-            // await context.Publish(...);
-
-            // Request a timeout: https://docs.particular.net/nservicebus/sagas/timeouts
-            // await RequestTimeout<MyCustomTimeout>(context, TimeSpan.FromMinutes(10));
-
-            // Ending a saga: https://docs.particular.net/nservicebus/sagas/#ending-a-saga
-            // MarkAsComplete();
-        }
-
-        public async Task Timeout(MyCustomTimeout timeout, IMessageHandlerContext context)
-        {
-            // Remove if saga does not require timeouts
-        }
+        // https://docs.particular.net/nservicebus/sagas/message-correlation
+        mapper.MapSaga(saga => saga.CorrelationId)
+            .ToMessage<MessageType1>(message => message.CorrelationId)
+            .ToMessage<MessageType2>(message => message.CorrelationId);
     }
 
-    internal class SagaNameData : ContainSagaData
+    public async Task Handle(MessageType1 message, IMessageHandlerContext context)
     {
-        public string CorrelationId { get; set; }
-        // Other properties
+        // Business logic here
     }
 
-    internal class MyCustomTimeout
+    public async Task Handle(MessageType2 message, IMessageHandlerContext context)
     {
-        // Optional extra properties
+        // Update saga data: https://docs.particular.net/nservicebus/sagas/#long-running-means-stateful
+        // this.Data.Property = ...
+
+        // Sending commands: https://docs.particular.net/nservicebus/messaging/send-a-message#inside-the-incoming-message-processing-pipeline
+        // await context.Send(...);
+
+        // Publishing events https://docs.particular.net/nservicebus/messaging/publish-subscribe/publish-handle-event
+        // await context.Publish(...);
+
+        // Request a timeout: https://docs.particular.net/nservicebus/sagas/timeouts
+        // await RequestTimeout<MyCustomTimeout>(context, TimeSpan.FromMinutes(10));
+
+        // Ending a saga: https://docs.particular.net/nservicebus/sagas/#ending-a-saga
+        // MarkAsComplete();
     }
+
+    public async Task Timeout(MyCustomTimeout timeout, IMessageHandlerContext context)
+    {
+        // Remove if saga does not require timeouts
+    }
+}
+
+internal class SagaNameData : ContainSagaData
+{
+    public string CorrelationId { get; set; }
+    // Other properties
+}
+
+internal class MyCustomTimeout
+{
+    // Optional extra properties
 }
