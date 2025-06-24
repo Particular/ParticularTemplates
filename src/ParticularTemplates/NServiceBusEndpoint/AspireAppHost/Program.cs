61ece5e2e62405ca95e79033128fb5ceb854e26e
Jayanthi <jayanthi.sourirajan@particular.net>
2025-06-24 14:21:30 -0700

    add boiler plate for rabbitmq  transport and postgresql persistence

--- src/ParticularTemplates/NServiceBusEndpoint/AspireAppHost/Program.cs
+++ src/ParticularTemplates/NServiceBusEndpoint/AspireAppHost/Program.cs
@@ -2,4 +2,21 @@
 // Add services .
 // builder.AddProject<Projects.MyServiceA>("servicea");
 // builder.AddProject<Projects.MyServiceB>("serviceb");
+
+#if (transport == "RabbitMQ")
+var transport = builder.AddRabbitMQ("transport")
+    .WithManagementPlugin(15672)
+    .WithUrlForEndpoint("management", url => url.DisplayText = "RabbitMQ Management");
+#endif
+
+#if (persistence == "PostgreSQL")
+var database = builder.AddPostgres("database");
+
+database.WithPgAdmin(resource =>
+{
+    resource.WithParentRelationship(database);
+    resource.WithUrlForEndpoint("http", url => url.DisplayText = "pgAdmin");
+});
+#endif
+
 builder.Build().Run();
